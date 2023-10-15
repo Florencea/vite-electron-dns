b6cbbed9640103c4a16a19bf9147f8dc1721c250
Florencea <bearflorencea@gmail.com>
2023-10-16 01:54:31 +0800

    feat: simply hooks

--- src/hooks/useDoh.tsx
+++ src/hooks/useDoh.tsx
@@ -1,26 +1,18 @@
-import { useLocalStorageState } from "ahooks";
+import {
+  ClearOutlined,
+  LoadingOutlined,
+  SwapRightOutlined,
+} from "@ant-design/icons";
 import { App, ButtonProps } from "antd";
 import { InputProps } from "antd/es/input";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import useDohRequest from "./useDohRequest";
 
 const useDoh = (servers: { title: string; server: string }[]) => {
   const { modal } = App.useApp();
-  const [host, setHost] = useLocalStorageState("dns_hosts_doh", {
-    defaultValue: "",
-  });
-  const [resultsA, setResultsA] = useLocalStorageState<string[]>(
-    "dns_hosts_result_doh_A",
-    {
-      defaultValue: [],
-    },
-  );
-  const [resultsAAAA, setResultsAAAA] = useLocalStorageState<string[]>(
-    "dns_hosts_result_doh_AAAA",
-    {
-      defaultValue: [],
-    },
-  );
+  const [host, setHost] = useState("");
+  const [resultsA, setResultsA] = useState([]);
+  const [resultsAAAA, setResultsAAAA] = useState([]);
   const [enabled, setEnabled] = useState(false);
   const { isFetching: isFetchingA } = useDohRequest(
     ["dohA"],
@@ -64,30 +56,68 @@ const useDoh = (servers: { title: string; server: string }[]) => {
       },
     },
   );
-  const inputProps: InputProps = {
-    value: host,
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-      setHost(e.target.value);
-    },
-  };
-  const buttonProps: ButtonProps = {
-    onClick: () => {
-      setEnabled(true);
-    },
-  };
-  const clearAll = () => {
+
+  const isLoading = isFetchingA || isFetchingAAAA;
+
+  const clearAll = useCallback(() => {
     setHost("");
     setResultsA([]);
     setResultsAAAA([]);
-  };
+  }, []);
+
+  const inputProps: InputProps = useMemo(
+    () => ({
+      title: `Domain Name\n(Single)`,
+      className: "w-[300px]",
+      allowClear: true,
+      disabled: isLoading,
+      value: host,
+      onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+        setHost(e.target.value);
+      },
+      onKeyDown: (e) => {
+        if (e.code === "Enter") {
+          setEnabled(true);
+        }
+      },
+    }),
+    [host, isLoading],
+  );
+
+  const btnSubmitProps: ButtonProps = useMemo(
+    () => ({
+      type: "primary",
+      disabled: isLoading || !host,
+      onClick: () => {
+        setEnabled(true);
+      },
+      children: isLoading ? <LoadingOutlined /> : <SwapRightOutlined />,
+    }),
+    [host, isLoading],
+  );
+
+  const btnClearProps: ButtonProps = useMemo(
+    () => ({
+      danger: true,
+      disabled: isLoading,
+      onClick: () => {
+        clearAll();
+      },
+      children: <ClearOutlined />,
+    }),
+    [clearAll, isLoading],
+  );
+
   const results = useMemo(
     () => resultsA.map((a, idx) => [a, resultsAAAA[idx]].join("\n")),
     [resultsA, resultsAAAA],
   );
+
   return {
     host,
     inputProps,
-    buttonProps,
+    btnSubmitProps,
+    btnClearProps,
     servers,
     results,
     isLoading: isFetchingA || isFetchingAAAA,
--- src/pages/Doh.tsx
+++ src/pages/Doh.tsx
@@ -1,8 +1,3 @@
-import {
-  ClearOutlined,
-  LoadingOutlined,
-  SwapRightOutlined,
-} from "@ant-design/icons";
 import { Button } from "antd";
 import { Input } from "../components/Input";
 import { TextArea } from "../components/TextArea";
@@ -10,40 +5,26 @@ import useDoh from "../hooks/useDoh";
 
 export default function Doh() {
   const {
-    host,
-    buttonProps,
+    btnSubmitProps,
+    btnClearProps,
     inputProps,
     servers,
     results,
     isLoading,
-    clearAll,
   } = useDoh([
-    { title: "Google", server: "https://dns.google/resolve" },
-    { title: "Cloudflare", server: "https://cloudflare-dns.com/dns-query" },
+    { title: "Google Public DNS", server: "https://dns.google/resolve" },
+    {
+      title: "Cloudflare 1.1.1.1",
+      server: "https://cloudflare-dns.com/dns-query",
+    },
   ]);
   return (
     <div className="w-full h-full p-4">
       <div className="w-full h-full flex justify-between items-stretch gap-2">
-        <Input
-          {...inputProps}
-          title={`Domain Name\n(Single)`}
-          className="w-[300px]"
-          allowClear
-          disabled={isLoading}
-        />
+        <Input {...inputProps} />
         <div className="self-center flex flex-col gap-3">
-          <Button {...buttonProps} type="primary" disabled={isLoading || !host}>
-            {isLoading ? <LoadingOutlined /> : <SwapRightOutlined />}
-          </Button>
-          <Button
-            danger
-            disabled={isLoading}
-            onClick={() => {
-              clearAll();
-            }}
-          >
-            <ClearOutlined />
-          </Button>
+          <Button {...btnSubmitProps} />
+          <Button {...btnClearProps} />
         </div>
 
         {servers.map(({ title, server }, idx) => (
