6a52ad79fcb9d0f8cfabd0ab9087993b8e867435
Florencea <bearflorencea@gmail.com>
2024-02-04 21:18:52 +0800

    fix: remove unnecessary config

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,8 +1,6 @@
 import { StyleProvider } from "@ant-design/cssinjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { App as AntApp, ConfigProvider, theme } from "antd";
-import zhTW from "antd/es/locale/zh_TW";
-import "dayjs/locale/zh-tw";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
@@ -33,19 +31,11 @@ createRoot(container).render(
     >
       <HashRouter>
         <ConfigProvider
-          locale={zhTW}
           theme={{
             algorithm: theme.darkAlgorithm,
             token: {
               colorPrimary: PRIMARY_COLOR,
-              colorPrimaryHover: PRIMARY_COLOR,
-              colorPrimaryActive: PRIMARY_COLOR,
               colorInfo: PRIMARY_COLOR,
-              colorInfoHover: PRIMARY_COLOR,
-              colorInfoActive: PRIMARY_COLOR,
-              colorLink: PRIMARY_COLOR,
-              colorLinkHover: PRIMARY_COLOR,
-              colorLinkActive: PRIMARY_COLOR,
             },
           }}
           autoInsertSpaceInButton={false}
