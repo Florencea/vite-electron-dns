5ff8af4116d9b962897c3262c26d6faf4933d44f
Florencea <bearflorencea@gmail.com>
2024-12-30 23:36:56 +0800

    fix: scroll

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -40,7 +40,11 @@ export const App = () => {
   const isLoading = ipv4.isPending || ipv6.isPending;
 
   return (
-    <Provider locale="en-US" theme={defaultTheme} UNSAFE_className="h-svh">
+    <Provider
+      locale="en-US"
+      theme={defaultTheme}
+      UNSAFE_className="h-svh overflow-y-scroll"
+    >
       <View padding="size-200" paddingTop="size-200">
         <Flex direction="column" gap="size-100">
           <View
